344473190fe97475481e0cf7a1b9f4155957c6b8
RizkyPratama05 <rizkypp428@gmail.com>
2025-10-24 15:30:06 +0700

    menambahkan gambar di dalam card

--- app/src/main/java/com/example/pertemuan4pam/UIPertama.kt
+++ app/src/main/java/com/example/pertemuan4pam/UIPertama.kt
@@ -1,6 +1,7 @@
 package com.example.pertemuan4pam
 
 import android.app.Activity
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -8,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
@@ -15,6 +17,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -43,8 +46,20 @@ fun ActivitasPertama(modifier: Modifier) {
         Card(modifier = Modifier
             .fillMaxWidth(fraction = 1f)
             .padding(all = 12.dp),
-            colors = CardDefaults.cardColors(containerColor = Color.DarkGray)) {
+            colors = CardDefaults.cardColors(containerColor = Color.DarkGray)
+        ){
+            Row(
+                verticalAlignment = Alignment.CenterVertically){
+                val gambar = painterResource(id = R.drawable.umy)
+                Image(
+                    painter = gambar,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(size = 100.dp)
+                        .padding(all = 5.dp)
+                )
 
+            }
         }
     }
 }
