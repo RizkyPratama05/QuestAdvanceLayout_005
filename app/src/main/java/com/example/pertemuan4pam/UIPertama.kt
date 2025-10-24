40baa9d8a9fe361d95eaa64b6d1f1299db3e3834
RizkyPratama05 <rizkypp428@gmail.com>
2025-10-24 15:34:09 +0700

    menambahkan spacer

--- app/src/main/java/com/example/pertemuan4pam/UIPertama.kt
+++ app/src/main/java/com/example/pertemuan4pam/UIPertama.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
@@ -59,6 +60,7 @@ fun ActivitasPertama(modifier: Modifier) {
                         .padding(all = 5.dp)
                 )
 
+                Spacer(modifier = Modifier.width(width = 1.dp))
             }
         }
     }
